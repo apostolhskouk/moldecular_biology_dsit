13533a82d100bafcfa79dfd555c310cbb9187d90
apostolhskouk <a.koukouvinis@athenarc.gr>
2025-05-21 10:17:08 +0300

    Delete extend directory

--- extend/traine_wave_pde_prop.sh
+++
@@ -1,8 +0,0 @@
-for pde in wave hj; do
-  for prop in plogp; do
-    #  for prop in 1err 2iik; do
-    python -m extend.train_wave_pde_prop \
-      --prop $prop \
-      --model.pde_function $pde 
-  done
-done
